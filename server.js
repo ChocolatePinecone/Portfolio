7a2e98e1eeae18720d9197a4fa3caa5dfc2f7116
Jelmer Pijnappel <chocolatepinecone@gmail.com>
2021-10-11 00:08:55 +0200

    Adjusted used port by node to listen to the process of heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("server is running on port 3000");
     console.log("Open your browser and hit url 'localhost:3000'");
 });
